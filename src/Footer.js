537dc9b22abacdf6cfd9f40f743989b55e528b4c
eva montgomery <eva.m.montgomery@gmail.com>
2020-02-18 15:41:07 -0500

    adds starter css

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Footer() {
     return(
-        <footer>
+        <footer className="footer">
         
         <a href="mailto:eva.m.montgomery@gmail.com">Questions? E-Mail me!</a>
           <div>2020 All rights reserved</div>
--- src/Header.js
+++ src/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Header(){
     return(
-        <header>
+        <header className="header">
         <h1>It's Wine-O-Clock</h1>
       </header>
     );
