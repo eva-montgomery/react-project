d19eaad18e727e707a20f189d300d69f23b19ba1
Hayes Crowley <froglegg@Hayess-MacBook-Pro.local>
2020-03-28 16:51:15 -0400

    added revokeURL method and UX logic in wineForm.js, image upload should work now. Added reccomendations in hayesNotes.md

--- src/components/WineForm.js
+++ src/components/WineForm.js
@@ -1,136 +1,174 @@
-import React from 'react';
-import {
-    Redirect
-  } from "react-router-dom"; 
-import WineRating from '../WineRating'
+import React from "react";
+import { Redirect } from "react-router-dom";
+import WineRating from "../WineRating";
 
 export default class WineForm extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            wine: '',
-            type: '',
-            price: '',
-            shop: '',
-            label: '',
-            comment: '',
-            rating: 5,
-
-        };
-    }
-    render() {
-        return (
-            <div>
-            <div className="main-content">
+  constructor(props) {
+    super(props);
+    this.state = {
+      wine: "",
+      type: "",
+      price: "",
+      shop: "",
+      label: "",
+      comment: "",
+      rating: 5
+    };
+  }
+  render() {
+    return (
+      <div>
+        <div className="main-content">
           <h3>Hi Wine-Lover 🍷</h3>
           <br></br>
-        <p>Welcome to Wine-O-Clock - your personal wine rating application!</p> 
-       
-        <p>Add the wines you bought to the form below and give them a wine-rating from 1-5, so every time you buy wine, you can look in your app and check if you have already tried it!
-        <p>To view all rated wines by all users, you can go to "All rated wines" in the navigation bar. See a wine you want to try? Add it to your favorites list.</p>
-        </p>
-        <h4>And remember: It's Wine-O-Clock somewhere and life is too short to drink shitty wine! Cheers!</h4><br></br>
-        <p>💜 Please donate an amount of what you saved, by only buying good wine, to an animal rescue 💜
-        </p>
-      
+          <p>
+            Welcome to Wine-O-Clock - your personal wine rating application!
+          </p>
+
+          <p>
+            Add the wines you bought to the form below and give them a
+            wine-rating from 1-5, so every time you buy wine, you can look in
+            your app and check if you have already tried it!
+            <p>
+              To view all rated wines by all users, you can go to "All rated
+              wines" in the navigation bar. See a wine you want to try? Add it
+              to your favorites list.
+            </p>
+          </p>
+          <h4>
+            And remember: It's Wine-O-Clock somewhere and life is too short to
+            drink shitty wine! Cheers!
+          </h4>
+          <br></br>
+          <p>
+            💜 Please donate an amount of what you saved, by only buying good
+            wine, to an animal rescue 💜
+          </p>
+        </div>
+        <div className="wine-form-container">
+          <div className="wine-box">
+            <form onSubmit={this._handleSubmit} id="wineForm">
+              <input
+                className="wine-input"
+                onChange={this._handleWine}
+                value={this.state.wine}
+                placeholder="Wine"
+              />
+              <input
+                className="wine-input"
+                onChange={this._handleType}
+                value={this.state.type}
+                placeholder="Type"
+              />
+              <input
+                className="wine-input"
+                onChange={this._handlePrice}
+                value={this.state.price}
+                placeholder="Price"
+              />
+              <input
+                className="wine-input"
+                onChange={this._handleShop}
+                value={this.state.shop}
+                placeholder="Bought at"
+              />{" "}
+              <br></br>
+              <label for="wine-input">Upload Wine Label:</label>
+              <br></br>
+              <input
+                type="file"
+                className="wine-input-label"
+                accept="image/png, image/jpeg, image/jpg"
+                onChange={this._handleLabel}
+                // value={this.state.label}
+              />
+              <input
+                className="wine-input"
+                onChange={this._handleComment}
+                value={this.state.comment}
+                placeholder="Comments"
+              />
+              <WineRating
+                className="wine-glasses"
+                onChange={this._handleRating}
+              />
+              <input
+                type="submit"
+                value="Add a new wine"
+                className="wine-submit"
+              />
+            </form>
+            {this.props.loginStatus.isLoggedIn == false ? (
+              <Redirect to="/" />
+            ) : (
+              ""
+            )}
+          </div>
+        </div>
       </div>
-            <div className="wine-form-container">
-            <div className="wine-box"> 
-                <form onSubmit={this._handleSubmit}>
-        
-                <input className="wine-input"
-                     onChange={this._handleWine}
-                    value={this.state.wine} placeholder="Wine"
-                    /> 
-                <input className="wine-input"
-                     onChange={this._handleType}
-                    value={this.state.type} placeholder="Type"
-                    />     
+    );
+  }
+  _handleSubmit = event => {
+    event.preventDefault();
+    this.props.handleSubmit(this.state);
+    this.setState(
+      {
+        wine: "",
+        type: "",
+        price: "",
+        shop: "",
+        label: "",
+        comment: "",
+        rating: 5
+      },
+      () => {
+        document.getElementById("wineForm").reset();
+      }
+    );
+  };
 
-                <input className="wine-input"
-                     onChange={this._handlePrice}
-                    value={this.state.price} placeholder="Price"
-                    />  
-                <input className="wine-input"
-                     onChange={this._handleShop}
-                    value={this.state.shop} placeholder="Bought at"
-                    /> <br></br>
+  _handleWine = event => {
+    this.setState({
+      wine: event.target.value
+    });
+  };
+  _handleType = event => {
+    this.setState({
+      type: event.target.value
+    });
+  };
+  _handlePrice = event => {
+    this.setState({
+      price: event.target.value
+    });
+  };
+  _handleShop = event => {
+    this.setState({
+      shop: event.target.value
+    });
+  };
 
-                   <label for="wine-input">Upload Wine Label:</label>
-                   <br></br><input type="file" className="wine-input-label" 
-                       accept="image/png, image/jpeg, image/jpg"
-                     onChange={this._handleLabel}
-                    // value={this.state.label} 
-                    />   
-                <input className="wine-input"
-                     onChange={this._handleComment}
-                    value={this.state.comment} placeholder="Comments"
-                    />  
+  _handleLabel = event => {
+    let file = event.target.files[0];
+    this.setState(
+      {
+        label: URL.createObjectURL(file)
+      },
+      () => {
+        URL.revokeObjectURL(file);
+      }
+    );
+  };
 
-                <WineRating className="wine-glasses" onChange={this._handleRating}/>
+  _handleRating = rating => {
+    this.setState({
+      rating
+    });
+  };
 
-                    <input type="submit" value="Add a new wine" className="wine-submit" />
-                    
-
-                </form>
-                {this.props.loginStatus.isLoggedIn == false ? <Redirect to ="/"/> : ""}   
-
-            </div>
-            </div>
-            </div>
-        )
-    }
-    _handleSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state)
-        this.props.handleSubmit(this.state);
-        this.setState({
-                wine: '',
-                type: '',
-                price: '',
-                shop: '',
-                label: '',
-                comment: '',
-                rating: 5})
-    }
-
-     
-    _handleWine = (event) => {
-        this.setState({
-            wine: event.target.value
-        });
-    }
-    _handleType = (event) => {
-        this.setState({
-            type: event.target.value
-        });       
-    }
-    _handlePrice = (event) => {
-        this.setState({
-            price: event.target.value
-        });
-    }
-    _handleShop = (event) => {
-        this.setState({
-            shop: event.target.value
-        });
-    }    
-    _handleLabel = (event) => {
-        this.setState({
-            // label: event.target.value
-            label: URL.createObjectURL(event.target.files[0])
-        });
-    }     
-    
-    _handleRating = (rating) => {
-        this.setState({
-            rating
-        });
-    }        
-
-    _handleComment = (event) => {
-        this.setState({
-            comment: event.target.value
-        });
-    }        
+  _handleComment = event => {
+    this.setState({
+      comment: event.target.value
+    });
+  };
 }
